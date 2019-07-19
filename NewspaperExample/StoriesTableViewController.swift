bafccd3e7e80289817ced55a720ccf1fe0653130
Kevin M <kmarx@caltech.edu>
2019-07-19 09:54:51 -0700

    at step 11 of tutorial

---
+++ NewspaperExample/StoriesTableViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+struct Headline {
+
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+
+}
+
+class HeadlineTableView: UITableViewCell{
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var TopLabel: UILabel!
+    @IBOutlet weak var BottomLabel: UILabel!
+    
+    
+}
+
+class StoriesTableViewController: UITableViewController {
+
+    var headlines = [
+        Headline(id: 1, title: "Lorem Ipsum", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at molestie massa fringilla id.", image: "Apple"),
+        Headline(id: 2, title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non, finibus et diam. Morbi et felis a lacus pharetra blandit.", image: "Banana"),
+        Headline(id: 3, title: "In ac ante sapien", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc in ipsum vehicula accumsan quis sit amet quam. Sed vel feugiat eros.", image: "Cantaloupe"),
+        ]
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+
+        let headline = headlines[indexPath.row]
+        cell.textLabel?.text = headline.title
+        cell.detailTextLabel?.text = headline.text
+        cell.imageView?.image = UIImage(named: headline.image)
+
+        return cell
+    }
+
+}
